fe3527f692cbbf5cd7a43143e0397137255c894f
Leo Neto <oleoneto@gmail.com>
2020-07-22 20:49:50 -0400

    Add settings module

--- vestlus/__init__.py
+++ vestlus/__init__.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 from .requires import REQUIRED_APPS
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __license__ = 'BSD 3-Clause'
 __copyright__ = 'Copyright 2020 Lehvitus ÖU'
 
---
+++ vestlus/settings.py
@@ -0,0 +1,11 @@
+# vestlus:settings
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': os.environ.get('ELASTICSEARCH_URL', 'http://127.0.0.1:9200/'),
+        'INDEX_NAME': 'haystack',
+    },
+}
